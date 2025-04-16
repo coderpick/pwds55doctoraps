0ebfbf24069301315b6e6dbd2058ba78c8adc91e
Hafizur Rahman <coderpickbd@gmail.com>
2025-04-16 15:25:54 +0600

    database sql file add

--- pwds_doctor_appointment.sql
+++ pwds_doctor_appointment.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Apr 11, 2025 at 06:52 AM
--- Server version: 8.0.30
--- PHP Version: 8.3.17
+-- Host: localhost
+-- Generation Time: Apr 16, 2025 at 09:25 AM
+-- Server version: 8.0.40
+-- PHP Version: 8.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,30 +24,94 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int NOT NULL,
+  `doctor_id` int NOT NULL,
+  `patient_id` int NOT NULL,
+  `schedule_id` int NOT NULL,
+  `appointment_number` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `reason` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Booked','In_Process','Completed','Cancel') COLLATE utf8mb3_unicode_ci NOT NULL,
+  `doctor_comment` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `doctors`
 --
 
 CREATE TABLE `doctors` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `address` mediumtext COLLATE utf8mb3_unicode_ci,
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `address` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci,
   `dob` date NOT NULL,
-  `expert_in` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `degree` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `gender` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `status` enum('Active','Inactive') COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Active',
-  `created_at` timestamp NULL DEFAULT NULL
+  `expert_in` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `degree` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Active','Inactive') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Active',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`id`, `user_id`, `phone`, `image`, `address`, `dob`, `expert_in`, `degree`, `gender`, `status`, `created_at`) VALUES
-(1, 5, '01739981172', 'uploads/174435233567f8b44f5291a.png', 'Dhaka', '1990-04-16', 'Cardiologist', 'MBBS', 'Male', 'Active', NULL),
-(2, 6, '01755554550', 'uploads/174435257567f8b53fb525f.png', 'Recusandae Laudanti', '1985-04-07', 'Laboriosam nihil im', 'Perferendis in sunt', 'Male', 'Active', NULL);
+INSERT INTO `doctors` (`id`, `user_id`, `name`, `phone`, `image`, `address`, `dob`, `expert_in`, `degree`, `gender`, `status`, `created_at`) VALUES
+(1, 5, 'Monir hossain', '01939981172', 'uploads/174435233567f8b44f5291a.png', 'Dhaka', '1990-04-16', 'Cardiologist', 'MBBS', 'Male', 'Active', NULL),
+(2, 6, 'Ginger Bell', '01755554550', 'uploads/174443533567f9f88767283.jpg', 'Recusandae Laudanti', '1985-04-07', 'Laboriosam nihil im', 'Perferendis in sunt', 'Male', 'Active', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `phone` varchar(15) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `address` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  `marital_status` enum('Single','Married') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`id`, `user_id`, `name`, `phone`, `address`, `gender`, `dob`, `marital_status`, `created_at`) VALUES
+(1, 16, 'Hall Leon', '01725697440', 'Dhaka', 'Male', '1986-05-09', 'Single', '2025-04-16 02:59:39'),
+(2, 17, 'Caryn Hayden', '01755555444', 'Repudiandae laborum', 'Female', '1970-08-14', 'Married', '2025-04-16 03:00:17'),
+(3, 19, 'Brody Graham', '01923569874', 'Natore', 'Male', '1988-10-11', 'Married', '2025-04-16 03:14:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int NOT NULL,
+  `doctor_id` int NOT NULL,
+  `schedule_date` date NOT NULL,
+  `schedule_day` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `consulting_time` int NOT NULL,
+  `status` enum('Available','Booked') COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Available',
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -57,11 +121,11 @@ INSERT INTO `doctors` (`id`, `user_id`, `phone`, `image`, `address`, `dob`, `exp
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `status` enum('Active','Inacitve') COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Active',
-  `type` enum('admin','doctor','patient') COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'patient'
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Active','Inacitve') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Active',
+  `type` enum('admin','doctor','patient') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'patient'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
@@ -71,12 +135,24 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `type`) VALUES
 (1, 'Mr. Admin', 'admin@gmail.com', '$2y$10$Nw.qJbsJw4Py8qswWTS55ut4G/kH9Cjy9bbB6kMoBNPP5ep4IpejG', 'Active', 'admin'),
 (2, 'Rubel', 'rubel@gmail.com', '$2y$10$9.cAAv6rFG.olsoax5ceC.Q3z41jvbo.VugDPxae2y./YQtQBszNu', 'Active', 'patient'),
-(5, 'Hafizur Rahman', 'hafizur@gmail.com', '$2y$10$DGj8LVHa9zcVY1XjxuRQueZY5STGMxuVKIW9xy2NcOtcDcTP/VV3m', 'Active', 'doctor'),
-(6, 'Ginger Bell', 'wynohaboxy@mailinator.com', '$2y$10$yaKmZpzV17YFjdsOCXuln.07HJ0ibGQsnhPHXfVhnNpEStRjFI9G2', 'Active', 'doctor');
+(5, 'Monir hossain', 'hafizur@gmail.com', '$2y$10$DGj8LVHa9zcVY1XjxuRQueZY5STGMxuVKIW9xy2NcOtcDcTP/VV3m', 'Active', 'doctor'),
+(6, 'Ginger Bell', 'wynohaboxy@mailinator.com', '$2y$10$yaKmZpzV17YFjdsOCXuln.07HJ0ibGQsnhPHXfVhnNpEStRjFI9G2', 'Active', 'doctor'),
+(16, 'Hall Leon', 'hexequ@mailinator.com', '$2y$10$7AUAu1VG7FRybp7LSAiBeuXsPuX1kMP2UnSKHEH4efHAyqQwAX.Ty', 'Active', 'patient'),
+(17, 'Caryn Hayden', 'fuqyq@mailinator.com', '$2y$10$DwJqeyVd1flqvad/oxbOVOztY6AEiYJlwOSOP3Vkge6CcAzuNHrky', 'Active', 'patient'),
+(19, 'Brody Graham', 'kamyqulo@mailinator.com', '$2y$10$lGpeCRO6nP6gbpKGIaQO4uJH91ZcheHF.NfkKFH1zKG.XTcT5ihKO', 'Active', 'patient');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `appointments_doctor_id_index` (`doctor_id`),
+  ADD KEY `appointments_patient_id_index` (`patient_id`),
+  ADD KEY `appointments_schedule_id_index` (`schedule_id`);
 
 --
 -- Indexes for table `doctors`
@@ -84,6 +160,20 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `type`) VALUES
 ALTER TABLE `doctors`
   ADD PRIMARY KEY (`id`),
   ADD KEY `doctors_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patients_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schedules_doctor_id_index` (`doctor_id`);
 
 --
 -- Indexes for table `users`
@@ -96,26 +186,64 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `doctors`
 --
 ALTER TABLE `doctors`
   ADD CONSTRAINT `doctors_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `patients`
+--
+ALTER TABLE `patients`
+  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
